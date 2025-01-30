45a7bf943a8dd529f2e671b08b025d97aa2453fd
dhdanudahlan <dhdanudahlan@gmail.com>
2025-01-30 14:37:57 +0700

    Fix: Moved AndroidApp and fix AndroidEntryPoint

--- app/src/main/java/com/aetherized/smartfinance/ui/MainActivity.kt
+++ app/src/main/java/com/aetherized/smartfinance/ui/MainActivity.kt
@@ -6,14 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.aetherized.smartfinance.ui.MainActivityViewModel
 import com.aetherized.smartfinance.ui.theme.SmartFinanceTheme
 import dagger.hilt.android.AndroidEntryPoint
 
