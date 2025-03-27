8e00ddf2dff3361168647aba0cf55a5ac689a8a6
Dimas Humayun Danu Dahlan <dhdanudahlan@gmail.com>
2025-03-28 01:08:24 +0700

    Change the viewmodel into event-driven, and fix how the data is fetched.
    Change viewmodel from direct-call to event-driven. Also updated data fetching and how the state is updated to make sure the data is properly fetched and stored

--- app/src/main/java/com/aetherized/smartfinance/features/finance/presentation/TransactionsViewModel.kt
+++ app/src/main/java/com/aetherized/smartfinance/features/finance/presentation/TransactionsViewModel.kt
@@ -6,180 +6,266 @@ import androidx.lifecycle.viewModelScope
 import com.aetherized.smartfinance.features.finance.domain.model.Category
 import com.aetherized.smartfinance.features.finance.domain.model.CategoryType
 import com.aetherized.smartfinance.features.finance.domain.model.DailySummary
+import com.aetherized.smartfinance.features.finance.domain.model.MonthlySummary
 import com.aetherized.smartfinance.features.finance.domain.model.Transaction
 import com.aetherized.smartfinance.features.finance.domain.usecase.DeleteTransactionUseCase
 import com.aetherized.smartfinance.features.finance.domain.usecase.GetCategoriesUseCase
-import com.aetherized.smartfinance.features.finance.domain.usecase.GetTransactionsUseCase
+import com.aetherized.smartfinance.features.finance.domain.usecase.GetMonthlyTransactionsUseCase
+import com.aetherized.smartfinance.features.finance.domain.usecase.GetYearlyTransactionsUseCase
 import com.aetherized.smartfinance.features.finance.domain.usecase.UpsertTransactionUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.retryWhen
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import java.time.LocalDate
 import java.time.YearMonth
 import javax.inject.Inject
 
+sealed interface TransactionsEvent {
+    object NavigateToPreviousMonth : TransactionsEvent
+    object NavigateToNextMonth : TransactionsEvent
+    object NavigateToPreviousYear : TransactionsEvent
+    object NavigateToNextYear : TransactionsEvent
+    data class FetchTransactions(val limit: Int = 50, val offset: Int = 0) : TransactionsEvent
+    data class ChangeTransactionsTab(val tab: TransactionsTab) : TransactionsEvent
+}
+
+sealed class SFAccount(val id: Long, val name: String) {
+    data object Imagination : SFAccount(id = 0, name = "Imagination")
+    data object Cash : SFAccount(id = 1, name = "MyCash")
+    data object BankAccounts : SFAccount(id = 2, name = "MyBank")
+}
+
+
+
 @HiltViewModel
 class TransactionsViewModel @Inject constructor(
-    private val getTransactionsUseCase: GetTransactionsUseCase,
+    private val getMonthlyTransactionsUseCase: GetMonthlyTransactionsUseCase,
+    private val getYearlyTransactionsUseCase: GetYearlyTransactionsUseCase,
     private val getCategoriesUseCase: GetCategoriesUseCase,
     private val upsertTransactionUseCase: UpsertTransactionUseCase,
     private val deleteTransactionUseCase: DeleteTransactionUseCase
 ) : ViewModel() {
-    private val _uiState = MutableStateFlow<TransactionsUiState>(
-        TransactionsUiState.Loading
-    )
-    val uiState: StateFlow<TransactionsUiState> = _uiState
 
-    private var selectedCategory: Category? = null
-    private var selectedCategoryType: CategoryType = CategoryType.EXPENSE
-    private var currentYearMonth: YearMonth = YearMonth.now()
+    // State flows for transactions
+    private val _transactionsUiState = MutableStateFlow<TransactionsUiState>(
+        TransactionsUiState.Loading()
+    )
+    val transactionsUiState: StateFlow<TransactionsUiState> = _transactionsUiState
 
     init {
-        Log.d("UI", "TransactionsViewModel init")
-        loadTransactions()
+        onEvent(TransactionsEvent.FetchTransactions(limit = 100, offset = 0))
     }
 
-    private fun loadTransactions(limit: Int = 50, offset: Int = 0) {
-        _uiState.value = TransactionsUiState.Loading
-
+    fun onEvent(event: TransactionsEvent) {
+        when (event) {
+            TransactionsEvent.NavigateToPreviousMonth -> navigateToPreviousMonth()
+            TransactionsEvent.NavigateToNextMonth -> navigateToNextMonth()
+            TransactionsEvent.NavigateToPreviousYear -> navigateToPreviousYear()
+            TransactionsEvent.NavigateToNextYear -> navigateToNextYear()
+            is TransactionsEvent.FetchTransactions -> {
+                fetchCategories()
+                fetchTransactions(event.limit, event.offset)
+            }
+            is TransactionsEvent.ChangeTransactionsTab -> changeTransactionsTab(event.tab)
+        }
+    }
+    private fun fetchCategories() {
         viewModelScope.launch {
-            try {
-                combine(
-                    getTransactionsUseCase(
-                        categoryId = selectedCategory?.id,
-                        categoryType = if (selectedCategory == null) selectedCategoryType else null,
-                        limit = limit,
-                        offset = offset
-                    ),
-                    getCategoriesUseCase(
-                        categoryType = selectedCategoryType,
-                        limit = limit,
-                        offset = offset
-                    )
-                ) { transactions, categories ->
-                    val dailySummaries = computeDailySummaries(
-                        transactions = transactions,
-                        categories = categories,
-                        yearMonth = currentYearMonth
-                    )
-                    TransactionsUiState.Success(
-                        transactions = transactions,
-                        categories = categories,
-                        yearMonth = currentYearMonth,
-                        selectedCategory = selectedCategory,
-                        selectedCategoryType = selectedCategoryType,
-                        dailySummaries = dailySummaries
-                    )
-                }.collect { combinedUiState ->
-                    _uiState.value = combinedUiState
+            getCategoriesUseCase()
+                .retryWhen { _, attempt ->
+                    if (attempt < 5) {
+                        delay(1000)
+                        true
+                    } else {
+                        false
+                    }
                 }
+                .catch { e ->
+                    _transactionsUiState.update {
+                        TransactionsUiState.Error(e.message ?: "Error loading categories")
+                    }
+                }
+                .collect { categories ->
+                    _transactionsUiState.update { currentState ->
+                        when (currentState) {
+                            is TransactionsUiState.Loading -> {
+                                currentState.copy(categories = categories)
+                            }
 
-            } catch (e: Exception) {
-                _uiState.value = TransactionsUiState.Error(
-                    message = e.message ?: "Unknown error",
+                            is TransactionsUiState.Success -> {
+                                currentState.copy(categories = categories)
+                            }
+
+                            else -> {
+                                TransactionsUiState.Loading(categories = categories)
+                            }
+                        }
+                    }
+                }
+        }
+    }
+    private fun <T> combineTransactionAndCategories(
+        transactionsFlow: Flow<List<Transaction>>,
+        generateSummaries: (transactions: List<Transaction>, categories: List<Category>) -> T,
+        updateState: (transactions: List<Transaction>, categories: List<Category>, summaries: T) -> TransactionsUiState
+    ) {
+        viewModelScope.launch {
+            combine(
+                transactionsFlow,
+                getCategoriesUseCase()
+            ) { transactions, categories ->
+                val summaries = generateSummaries(transactions, categories)
+
+                updateState(transactions, categories, summaries)
+            }
+                .catch { e ->
+                    _transactionsUiState.value = TransactionsUiState.Error(
+                        message = e.message ?: "Error loading transactions",
+                    )
+                }
+                .collect { newUiState ->
+                    _transactionsUiState.update { newUiState }
+                }
+        }
+    }
+    private fun fetchTransactions(limit: Int = 50, offset: Int = 0) {
+        viewModelScope.launch {
+            if (transactionsUiState.value.filterState.selectedTab == TransactionsTab.Daily) {
+                val transactionsFlow = getMonthlyTransactionsUseCase(
+                        yearMonth = transactionsUiState.value.filterState.yearMonth,
+                        limit = limit,
+                        offset = offset
+                    )
+                combineTransactionAndCategories(
+                    transactionsFlow = transactionsFlow,
+                    generateSummaries = { transactions, categories ->
+                        generateDailySummaries(
+                            transactions = transactions,
+                            categories = categories,
+                        )
+                    },
+                    updateState = { transactions, categories, summaries ->
+                        TransactionsUiState.Success(
+                            transactions = transactions,
+                            dailySummaries = summaries,
+                            filterState = transactionsUiState.value.filterState,
+                            categories = categories
+                        )
+                    }
+                )
+            } else if (transactionsUiState.value.filterState.selectedTab == TransactionsTab.Monthly) {
+                val transactionsFlow = getYearlyTransactionsUseCase(
+                    yearMonth = transactionsUiState.value.filterState.yearMonth,
+                    limit = limit,
+                    offset = offset
+                )
+                combineTransactionAndCategories(
+                    transactionsFlow = transactionsFlow,
+                    generateSummaries = { transactions, categories ->
+                        generateMonthlySummaries(
+                            transactions = transactions,
+                            categories = categories,
+                        )
+                    },
+                    updateState = { transactions, categories, summaries ->
+                        TransactionsUiState.Success(
+                            transactions = transactions,
+                            monthlySummaries = summaries,
+                            filterState = transactionsUiState.value.filterState,
+                            categories = categories
+                        )
+                    }
                 )
             }
         }
     }
 
-    /**
-     * Computes daily summaries: groups transactions by date (within the specified month)
-     * and calculates total income and expense for each day.
-     */
-    private fun computeDailySummaries(
-        transactions: List<Transaction>,
-        categories: List<Category>,
-        yearMonth: YearMonth
-    ): List<DailySummary> {
-        // Prepare the dates for the current month.
-        val daysInMonth = (yearMonth.lengthOfMonth()..1).map { day ->
-            LocalDate.of(yearMonth.year, yearMonth.month, day)
-        }
 
-        // Group transactions by date.
+    private fun generateDailySummaries(
+        transactions: List<Transaction> = emptyList(),
+        categories: List<Category> = emptyList()
+    ): List<DailySummary> {
+        val incomeId = categories.filter { it.type == CategoryType.INCOME }.map { it.id }
+        val expenseId = categories.filter { it.type == CategoryType.EXPENSE }.map { it.id }
+
+        // Group transactions by date
         val transactionsByDate = transactions.groupBy { it.timestamp.toLocalDate() }
 
-        return daysInMonth.mapNotNull { date ->
-            val dailyTransactions = transactionsByDate[date].orEmpty()
-            // Only include days that have transactions.
-            if (dailyTransactions.isNotEmpty()) {
-                // Get income and expense category IDs.
-                val incomeIds = categories.filter { it.type == CategoryType.INCOME }.map { it.id }.toSet()
-                val expenseIds = categories.filter { it.type == CategoryType.EXPENSE }.map { it.id }.toSet()
+        return transactionsByDate.map { (date, transactionsForDate) ->
+            val totalIncome = transactionsForDate.filter { it.categoryId in incomeId }.sumOf { it.amount }
+            val totalExpense = transactionsForDate.filter { it.categoryId in expenseId }.sumOf { it.amount }
 
-                val incomeTotal = dailyTransactions.filter { it.categoryId in incomeIds }.sumOf { it.amount }
-                val expenseTotal = dailyTransactions.filter { it.categoryId in expenseIds }.sumOf { it.amount }
+            DailySummary(date, transactionsForDate, totalIncome, totalExpense)
+        }
+    }
+    private fun generateMonthlySummaries(
+        transactions: List<Transaction> = emptyList(),
+        categories: List<Category> = emptyList()
+    ): List<MonthlySummary> {
+        val incomeId = categories.filter { it.type == CategoryType.INCOME }.map { it.id }
+        val expenseId = categories.filter { it.type == CategoryType.EXPENSE }.map { it.id }
 
-                DailySummary(
-                    date = date,
-                    transactions = dailyTransactions,
-                    income = incomeTotal,
-                    expense = expenseTotal
-                )
-            } else {
-                null
-            }
+        // Group transactions by date.
+        val transactionsByMonth = transactions.groupBy { YearMonth.from(it.timestamp) }
+
+        return transactionsByMonth.map { (month, transactionsForDate) ->
+            val totalIncome = transactionsForDate.filter { it.categoryId in incomeId }.sumOf { it.amount }
+            val totalExpense = transactionsForDate.filter { it.categoryId in expenseId }.sumOf { it.amount }
+
+            MonthlySummary(month, transactionsForDate, totalIncome, totalExpense)
         }
     }
 
     // Navigation actions to update the month.
-    fun previousMonth() {
-        currentYearMonth = currentYearMonth.minusMonths(1)
-        loadTransactions()
+    private fun navigateToPreviousMonth() {
+        Log.d("TransactionsViewModel", "navigateToPreviousMonth")
+        _transactionsUiState.update { currentState ->
+            val currentFilter = (currentState as? TransactionsUiState.Success)?.filterState ?: (currentState as? TransactionsUiState.Loading)?.filterState ?: FilterState()
+            TransactionsUiState.Loading(currentFilter.copy(yearMonth = currentFilter.yearMonth.minusMonths(1)))
+        }
+        fetchTransactions()
     }
 
-    fun nextMonth() {
-        currentYearMonth = currentYearMonth.plusMonths(1)
-        loadTransactions()
+    private fun navigateToNextMonth() {
+        Log.d("TransactionsViewModel", "navigateToNextMonth")
+        _transactionsUiState.update { currentState ->
+            val currentFilter = (currentState as? TransactionsUiState.Success)?.filterState ?: (currentState as? TransactionsUiState.Loading)?.filterState ?: FilterState()
+            TransactionsUiState.Loading(currentFilter.copy(yearMonth = currentFilter.yearMonth.plusMonths(1)))
+        }
+        fetchTransactions()
     }
 
-    fun onCategorySelected(category: Category?) {
-        selectedCategory = category
-        loadTransactions()
+    // Navigation actions to update the month.
+    private fun navigateToPreviousYear() {
+        Log.d("TransactionsViewModel", "navigateToPreviousYear")
+        _transactionsUiState.update { currentState ->
+            val currentFilter = (currentState as? TransactionsUiState.Success)?.filterState ?: (currentState as? TransactionsUiState.Loading)?.filterState ?: FilterState()
+            TransactionsUiState.Loading(currentFilter.copy(yearMonth = currentFilter.yearMonth.minusYears(1)))
+        }
+        fetchTransactions()
     }
 
-    fun onCategoryTypeSelected(type: CategoryType) {
-        selectedCategoryType = type
-        selectedCategory = null  // Reset selected category when type changes.
-        loadTransactions()
+    private fun navigateToNextYear() {
+        Log.d("TransactionsViewModel", "navigateToNextYear")
+        _transactionsUiState.update { currentState ->
+            val currentFilter = (currentState as? TransactionsUiState.Success)?.filterState ?: (currentState as? TransactionsUiState.Loading)?.filterState ?: FilterState()
+            TransactionsUiState.Loading(currentFilter.copy(yearMonth = currentFilter.yearMonth.plusYears(1)))
+        }
+        fetchTransactions()
     }
 
-//    fun createOrUpdateTransaction(
-//        id: Long?,
-//        categoryId: Long,
-//        amount: Int,
-//        note: String? = null
-//    ) {
-//        viewModelScope.launch {
-//            try {
-//                val newTransaction = Transaction(
-//                    id = id ?: 0L,
-//                    categoryId = categoryId,
-//                    amount = amount,
-//                    note = note,
-//                )
-//                upsertTransactionUseCase(newTransaction)
-//
-//                loadTransactions()
-//            } catch (e: Exception) {
-//                _uiState.value = TransactionsUIState.Error(
-//                    e.message ?: "Unable to upsert transaction"
-//                )
-//            }
-//        }
-//    }
-//
-//    fun deleteTransaction(transactionId: Long) {
-//        viewModelScope.launch {
-//            try {
-//                deleteTransactionUseCase(transactionId)
-//            } catch (e: Exception) {
-//                _uiState.value = TransactionsUIState.Error(
-//                    e.message ?: "Unable to delete transaction"
-//                )
-//            }
-//        }
-//    }
+    private fun changeTransactionsTab(tab: TransactionsTab = TransactionsTab.Daily) {
+        _transactionsUiState.update { currentState ->
+            val currentFilter = (currentState as? TransactionsUiState.Success)?.filterState ?: (currentState as? TransactionsUiState.Loading)?.filterState ?: FilterState()
+            TransactionsUiState.Loading(currentFilter.copy(selectedTab = tab))
+        }
+        fetchTransactions()
+
+    }
 }
