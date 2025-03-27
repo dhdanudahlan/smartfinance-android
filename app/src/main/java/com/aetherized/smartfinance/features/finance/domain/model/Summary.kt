2a7b4fda0b4a365e80eb09f812cdcb18b40723e0
Dimas Humayun Danu Dahlan <dhdanudahlan@gmail.com>
2025-03-27 19:29:16 +0700

    Rename file name and add new data class
    DailySummary is not suitable to be used for monthly summary data, hence a separate MonthlySummary is needed.

--- app/src/main/java/com/aetherized/smartfinance/features/finance/domain/model/Summary.kt
+++ app/src/main/java/com/aetherized/smartfinance/features/finance/domain/model/Summary.kt
@@ -1,10 +1,18 @@
 package com.aetherized.smartfinance.features.finance.domain.model
 
 import java.time.LocalDate
+import java.time.YearMonth
 
 data class DailySummary(
     val date: LocalDate,
     val transactions: List<Transaction>,
-    val income: Double,   // or Double if needed
+    val income: Double,
+    val expense: Double
+)
+
+data class MonthlySummary(
+    val yearMonth: YearMonth,
+    val transactions: List<Transaction>,
+    val income: Double,
     val expense: Double
 )
